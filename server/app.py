40361bf1a436d170a589f3467ae6417f98a7d4df
Michael McCormack <mcctwin@gmail.com>
2026-01-21 16:53:20 -0600

    added me, login, and JournalEntryIndex(get paginated, post) routes

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import request, jsonify, make_response
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, verify_jwt_in_request
 from config import app, db, api, jwt
 from models import User, UserSchema, JournalEntry, JournalEntrySchema
-from datetime import timedelta
+from datetime import datetime
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
@@ -39,3 +39,60 @@ class Signup(Resource):
 
         user_schema = UserSchema()
         return user_schema.dump(new_user), 201
+    
+class WhoAmI(Resource):
+    def get(self):
+        user_id = get_jwt_identity()
+        user = User.query.filter(User.id == user_id).first()
+        return UserSchema().dump(user), 200
+    
+class Login(Resource):
+    def post(self):
+        username = request.json['username']
+        password = request.json['password']
+
+        user = User.query.filter(User.username == username).first()
+        if user and user.authenticate(password):
+            token = create_access_token(identity=user.id)
+            return make_response(jsonify(token=token, user=UserSchema().dump(user)), 200)
+        else:
+            return {'errors': ['Invalid username or password']}, 401
+
+class JournalEntryIndex(Resource):
+    def get(self):
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 10, type=int)
+        pagination = JournalEntry.query.paginate(page, per_page, error_out=False)
+        journal_entries = pagination.items
+
+        return {
+            'page': page,
+            'per_page': per_page,
+            'total': pagination.total,
+            'total_pages': pagination.pages,
+            'journal_entries': [JournalEntrySchema().dump(je) for je in journal_entries]
+        }, 200
+    
+    def post(self):
+        request_json = request.get_json()
+        journal_entry = JournalEntry(
+            title=request_json.get('title'),
+            date=request_json.get('date'),
+            content=request_json.get('content'),
+            user_id=get_jwt_identity()
+        )
+        try:
+            db.session.add(journal_entry)
+            db.session.commit()
+            return JournalEntrySchema().dump(journal_entry), 201
+        except IntegrityError:
+            db.session.rollback()
+            return {'errors': ['Failed to create journal entry.']}, 422
+
+api.add_resource(Signup, '/signup', endpoint='/signup')
+api.add_resource(WhoAmI, '/me', endpoint='/me')
+api.add_resource(Login, '/login', endpoint='/login')
+api.add_resource(JournalEntryIndex, '/journal_entries', endpoint='/journal_entries')
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
